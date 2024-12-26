a097989167a7535b46c81b5d8117ef7861cedea8
MisikIvan <vt211_miv@student.ztu.edu.ua>
2024-12-26 13:30:13 +0200

    Lab1 rework

--- Lab.1/my-resume-app/src/components/CheckboxGroup.vue
+++ Lab.1/my-resume-app/src/components/CheckboxGroup.vue
@@ -1,31 +1,48 @@
-<!-- src/components/CheckboxGroup.vue -->
 <template>
-    <div class="mb-4">
-      <span class="block text-gray-700">{{ label }}</span>
-      <div class="mt-2">
-        <label v-for="option in options" :key="option.value" class="inline-flex items-center mr-4">
-          <input
-            type="checkbox"
-            :value="option.value"
-            v-model="modelValue"
-            class="form-checkbox h-4 w-4 text-indigo-600"
-          />
-          <span class="ml-2">{{ option.label }}</span>
-        </label>
-      </div>
-      <span v-if="errors" class="text-red-500 text-sm">{{ errors }}</span>
+  <div class="mb-4">
+    <span class="block text-gray-700">{{ label }}</span>
+    <div class="mt-2">
+      <label
+        v-for="option in options"
+        :key="option.value"
+        class="inline-flex items-center mr-4"
+      >
+        <input
+          type="checkbox"
+          :value="option.value"
+          :checked="modelValue.includes(option.value)"
+          @change="toggleValue(option.value)"
+          class="form-checkbox h-4 w-4 text-indigo-600"
+        />
+        <span class="ml-2">{{ option.label }}</span>
+      </label>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      label: String,
-      options: Array,
-      modelValue: Array,
-      errors: String,
+    <span v-if="errors" class="text-red-500 text-sm">{{ errors }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    options: Array,
+    modelValue: {
+      type: Array,
+      default: () => [],
     },
-    emits: ['update:modelValue'],
-  }
-  </script>
-  
+    errors: String,
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    toggleValue(value) {
+      const newValue = [...this.modelValue];
+      if (newValue.includes(value)) {
+        newValue.splice(newValue.indexOf(value), 1); // Remove value
+      } else {
+        newValue.push(value); // Add value
+      }
+      this.$emit("update:modelValue", newValue);
+    },
+  },
+};
+</script>
--- Lab.1/my-resume-app/src/components/DatePicker.vue
+++ Lab.1/my-resume-app/src/components/DatePicker.vue
@@ -1,26 +1,25 @@
-<!-- src/components/DatePicker.vue -->
 <template>
-    <div class="mb-4">
-      <label :for="id" class="block text-gray-700">{{ label }}</label>
-      <input
-        :id="id"
-        type="date"
-        v-model="modelValue"
-        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-      />
-      <span v-if="errors" class="text-red-500 text-sm">{{ errors }}</span>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      label: String,
-      id: String,
-      modelValue: String,
-      errors: String,
-    },
-    emits: ['update:modelValue'],
-  }
-  </script>
-  
+  <div class="mb-4">
+    <label :for="id" class="block text-gray-700">{{ label }}</label>
+    <input
+      :id="id"
+      type="date"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
+    />
+    <span v-if="errors" class="text-red-500 text-sm">{{ errors }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    id: String,
+    modelValue: String,
+    errors: String,
+  },
+  emits: ['update:modelValue'],
+};
+</script>
